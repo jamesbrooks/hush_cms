c77eb9793e17268f80edad95eb06e59f624a009f
James Brooks <james@gooddogdesign.com>
2008-08-19 12:02:07 +0930

    Commented out uninstall hook, for now

--- uninstall.rb
+++ uninstall.rb
@@ -1,4 +1,4 @@
-require 'fileutils'
- 
-hush_config = File.dirname(__FILE__) + '/../../../config/hush.yml'
-FileUtils.rm hush_config if File.exist?(hush_config)
+#require 'fileutils'
+# 
+#hush_config = File.dirname(__FILE__) + '/../../../config/hush.yml'
+#FileUtils.rm hush_config if File.exist?(hush_config)
