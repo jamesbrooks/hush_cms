07de64ba1a191414f5f2b4942c79d0cfbee772b1
James Brooks <james@gooddogdesign.com>
2008-08-19 14:57:49 +0930

    Added default admin route to be generated with hush_cms_admin in routes.rb

--- lib/extensions/mapper.rb
+++ lib/extensions/mapper.rb
@@ -3,10 +3,12 @@ module ActionController
     class RouteSet
       class Mapper
         def hush_cms_pages(path, options = {})          
-          named_route "hush_cms_page", path, :controller => 'hush_cms_pages', :action => 'show'
+          named_route 'hush_cms_page', path, :controller => 'hush_cms_pages', :action => 'show'
         end
         
         def hush_cms_admin(path)
+          named_route 'hush_cms_admin', path, :controller => 'hush_cms_admin', :action => 'index'
+          
           namespace :hush_cms_admin, :path_prefix => path do |a|
             a.resources :categories
             a.resources :pages
