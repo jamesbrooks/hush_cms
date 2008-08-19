a8a8de45ced3180bc7098d7a19ad6d4d7af21ff5
James Brooks <james@gooddogdesign.com>
2008-08-19 13:44:10 +0930

    Integrated initial loading into the HushCMS module, added loading of config/hush.yml and validation of required entries

--- lib/hush_cms.rb
+++ lib/hush_cms.rb
@@ -1,15 +1,43 @@
 require 'extensions/string'
 require 'extensions/mapper'
 
-%w( models controllers helpers ).each do |dir|
-  path = File.join(File.dirname(__FILE__), dir)
-  $LOAD_PATH << path
-  Dependencies.load_paths << path
-  Dependencies.load_once_paths.delete(path)
-end
-
-ActionController::Base.append_view_path(File.join(File.dirname(__FILE__), 'views'))
-ActionView::Base.send :include, HushCMSViewHelpers
-
 module HushCMS
+  class << self
+    attr_reader :configuration
+    
+    def load
+      %w( models controllers controllers/admin helpers ).each do |dir|
+        path = File.join(File.dirname(__FILE__), dir)
+        $LOAD_PATH << path
+        Dependencies.load_paths << path
+        Dependencies.load_once_paths.delete(path)
+      end
+    
+      ActionController::Base.append_view_path(File.join(File.dirname(__FILE__), 'views'))
+      ActionView::Base.send :include, HushCMSViewHelpers
+      
+      if File.exist?("#{RAILS_ROOT}/config/hush.yml")
+        @configuration = YAML::load(File.open("#{RAILS_ROOT}/config/hush.yml"))
+      else
+        raise ConfigurationException.new("config/hush.yml doesn't exist")
+      end
+      
+      validate_configuration
+    end
+    
+  private
+    def validate_configuration
+      unless configuration
+        raise ConfigurationException.new("config/hush.yml doesn't define anything")
+      end
+      
+      unless configuration['administration'] && configuration['administration']['username'] && configuration['administration']['password']
+        raise ConfigurationException.new("config/hush.yml doesn't define username and password under administration")
+      end
+    end
+  end
+  
+  class ConfigurationException < Exception
+  end
 end
+            
