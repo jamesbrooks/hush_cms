b1f84ccce04807f10939627699f97c85bc8819de
James Brooks <james@gooddogdesign.com>
2008-08-19 14:57:14 +0930

    renamed config directory to install, more appropriate (with stylesheets and javascripts to be copied over going in here)

--- install.rb
+++ install.rb
@@ -1,5 +1,5 @@
 require 'fileutils'
  
 hush_config = File.dirname(__FILE__) + '/../../../config/hush.yml'
-FileUtils.cp File.dirname(__FILE__) + '/config/hush.yml.tpl', hush_config unless File.exist?(hush_config)
-puts IO.read(File.join(File.dirname(__FILE__), 'config', 'INSTRUCTIONS'))
+FileUtils.cp File.dirname(__FILE__) + '/install/hush.yml.tpl', hush_config unless File.exist?(hush_config)
+puts IO.read(File.join(File.dirname(__FILE__), 'install', 'INSTRUCTIONS'))
