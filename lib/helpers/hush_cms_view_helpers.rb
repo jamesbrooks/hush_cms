3bf5b53c8092c388377389cfa1bc72bf263dac9f
James Brooks <james@gooddogdesign.com>
2008-08-25 16:09:09 +0930

    Added snippet view helper

--- lib/helpers/hush_cms_view_helpers.rb
+++ lib/helpers/hush_cms_view_helpers.rb
@@ -1,4 +1,8 @@
 module HushCMSViewHelpers
+  def hush_cms_snippet(name)
+    HushCMS::Snippet.find_by_name(name).content rescue "('#{name}' snippet not found)"    
+  end
+  
   def hush_cms_breadcrumbs(page)
     page.breadcrumbs.map do |page|
       link_to page.title, hush_cms_page_path(page.path)
