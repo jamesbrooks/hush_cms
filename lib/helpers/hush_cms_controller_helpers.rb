4261f1f24da4fb0206f56ffca826eb6cb569de35
James Brooks <james@gooddogdesign.com>
2008-09-03 16:13:11 +0930

    Updating hush_cms_posts_for helper, broken during the renaming of HushCMS::Category -> HushCMS::PostCategory

--- lib/helpers/hush_cms_controller_helpers.rb
+++ lib/helpers/hush_cms_controller_helpers.rb
@@ -1,5 +1,5 @@
 module HushCMSControllerHelpers
   def hush_cms_posts_for(category, options={})
-    HushCMS::Category.find_by_slug(category).posts.published.all(options) rescue []
+    HushCMS::PostCategory.find_by_slug(category).posts.published.all(options) rescue []
   end
 end
