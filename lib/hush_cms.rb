70d8981dcb27c7a727e634ef5f4ca734034a3fd3
James Brooks <james@gooddogdesign.com>
2009-03-11 14:45:09 +1030

    Fixed a problem with newer rails using Dependencies (Anonymous modules have no name to be referenced by)

--- lib/hush_cms.rb
+++ lib/hush_cms.rb
@@ -22,8 +22,8 @@ module HushCMS
       %w( models controllers controllers/admin helpers ).each do |dir|
         path = File.join(File.dirname(__FILE__), dir)
         $LOAD_PATH << path
-        Dependencies.load_paths << path
-        Dependencies.load_once_paths.delete(path)
+        ActiveSupport::Dependencies.load_paths << path
+        ActiveSupport::Dependencies.load_once_paths.delete(path)
       end
       
       # Load any user extensions
