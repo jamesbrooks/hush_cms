2deb0c5228f5b4059c45ed81b20f06602d0578b0
Chris Gooley <chris@White-Fawn.local>
2010-04-19 22:09:25 +0930

    added stylesheet links to install

--- install.rb
+++ install.rb
@@ -16,5 +16,7 @@ FileUtils.cp_r File.dirname(__FILE__) + '/install/stylesheets/hush_cms_admin.css
 FileUtils.cp_r File.dirname(__FILE__) + '/install/javascripts/hush_cms_date_picker.js',  date_javascripts unless File.exist?(date_javascripts)
 FileUtils.cp_r File.dirname(__FILE__) + '/install/stylesheets/hush_cms_date_picker.css', date_stylesheets unless File.exist?(date_stylesheets)
 FileUtils.cp_r File.dirname(__FILE__) + '/install/javascripts/hush_cms_date_lang',       date_languages   unless File.exist?(date_languages)
+FileUtils.cp_r File.dirname(__FILE__) + '/install/stylesheets/hush_cms_admin_navigation.css', nav_stylesheets unless File.exist?(nav_stylesheets)
+FileUtils.cp_r File.dirname(__FILE__) + '/install/stylesheets/hush_cms_ie_admin.css', ie_stylesheets unless File.exist?(ie_stylesheets)
 
 puts IO.read(File.join(File.dirname(__FILE__), 'install', 'INSTRUCTIONS'))
