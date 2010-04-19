536e0f925ecec5de2bbf1f45f4298b6687d86cfb
Chris Gooley <chris@White-Fawn.local>
2010-04-19 22:22:15 +0930

    forgot adding variables

--- install.rb
+++ install.rb
@@ -8,6 +8,8 @@ hush_stylesheets = "#{RAILS_ROOT}/public/stylesheets/hush_cms_admin.css"
 date_javascripts = "#{RAILS_ROOT}/public/javascripts/hush_cms_date_picker.js"
 date_stylesheets = "#{RAILS_ROOT}/public/stylesheets/hush_cms_date_picker.css"
 date_languages   = "#{RAILS_ROOT}/public/javascripts/hush_cms_date_lang"
+nav_stylesheets  = "#{RAILS_ROOT}/public/stylesheets/hush_cms_admin_navigations.css"
+ie_stylesheets   = "#{RAILS_ROOT}/public/stylesheets/hush_cms_ie_admin.css"
 
 FileUtils.cp   File.dirname(__FILE__) + '/install/hush.yml.tpl',                         hush_config      unless File.exist?(hush_config)
 FileUtils.cp_r File.dirname(__FILE__) + '/install/images',                               hush_images      unless File.exist?(hush_images)
